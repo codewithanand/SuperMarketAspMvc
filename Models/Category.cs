87d1833ad4c83e67cf4dbcab102c13c10b077a75
Anand Kumar <codebyaanand@gmail.com>
2024-05-07 22:51:20 +0530

    Tag Helpers

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperMarketMVC.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
