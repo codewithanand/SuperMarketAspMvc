fd5ad33e33d59382220c037ea927267ed3bdac3f
Anand Kumar <codebyaanand@gmail.com>
2024-05-08 22:52:35 +0530

    mvc notes

--- obj/Debug/net8.0/SuperMarketMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/SuperMarketMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuperMarketMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87d1833ad4c83e67cf4dbcab102c13c10b077a75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03d4fdcc8c5af5f33f40c29679e221bce867aace")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuperMarketMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuperMarketMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
